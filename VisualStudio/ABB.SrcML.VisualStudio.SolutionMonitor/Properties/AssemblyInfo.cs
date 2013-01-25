1ef402cbd32a226b20060f986afe58e0700ddbb1
Vinay Augustine <vinay.augustine@us.abb.com>
2013-01-25 15:21:28 -0500

    Modified SolutionMonitor to have a split properties file. Modified SolutionMonitor project to refer to the right build directory.

--- VisualStudio/ABB.SrcML.VisualStudio.SolutionMonitor/Properties/AssemblyInfo.cs
+++ VisualStudio/ABB.SrcML.VisualStudio.SolutionMonitor/Properties/AssemblyInfo.cs
@@ -1,36 +1,52 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿/******************************************************************************
+ * Copyright (c) 2011 ABB Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Vinay Augustine (ABB Group) - split to use SolutionInfo
+ *****************************************************************************/
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
+//
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//
 [assembly: AssemblyTitle("ABB.SrcML.VisualStudio.SolutionMonitor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("ABB.SrcML.VisualStudio.SolutionMonitor")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © ABB 2012")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("77d7f591-0691-469b-ac52-98282a632eff")]
-
-// Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// In order to sign your assembly you must specify a key to use. Refer to the 
+// Microsoft .NET Framework documentation for more information on assembly signing.
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Use the attributes below to control which key is used for signing. 
+//
+// Notes: 
+//   (*) If no key is specified - the assembly cannot be signed.
+//   (*) KeyName refers to a key that has been installed in the Crypto Service
+//       Provider (CSP) on your machine. 
+//   (*) If the key file and a key name attributes are both specified, the 
+//       following processing occurs:
+//       (1) If the KeyName can be found in the CSP - that key is used.
+//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
+//           in the file is installed into the CSP and used.
+//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
+//       documentation for more information on this.
+//
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
