a70a6ef3025b1c8fcfaaaf3705ad976ba1bd5bb4
Vinay Augustine <vinay.augustine@us.abb.com>
2013-02-05 16:33:56 -0500

    Added license text to test files.

--- ABB.SrcML.Data.Test/SrcMLFileUnitSetup.cs
+++ ABB.SrcML.Data.Test/SrcMLFileUnitSetup.cs
@@ -1,4 +1,15 @@
-﻿using ABB.SrcML.Utilities;
+﻿/******************************************************************************
+ * Copyright (c) 2013 ABB Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Vinay Augustine (ABB Group) - initial API, implementation, & documentation
+ *****************************************************************************/
+
+using ABB.SrcML.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
