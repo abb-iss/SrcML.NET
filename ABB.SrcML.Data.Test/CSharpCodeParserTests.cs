670c9dee96d3352b9abef49ce58c2e55513ac65b
patrickfrancis <patrick.francis@us.abb.com>
2013-03-04 08:23:10 -0500

    Added C# namespace tests

--- ABB.SrcML.Data.Test/CSharpCodeParserTests.cs
+++ ABB.SrcML.Data.Test/CSharpCodeParserTests.cs
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Vinay Augustine (ABB Group) - initial API, implementation, & documentation
+ *    Patrick Francis (ABB Group) - initial API, implementation, & documentation
  *****************************************************************************/
 
 using System;
@@ -49,22 +49,106 @@ namespace ABB.SrcML.Data.Test {
 
         [Test]
         public void TestCreateTypeDefinition_Namespace() {
-            //TODO: implement
-            
-            
-            
             ////Foo.cs
-            //public class Foo {
-            //    public int bar;
+            //namespace Example {
+            //    public class Foo {
+            //        public int bar;
+            //    }
             //}
-            string fooXml = @"<class><specifier>public</specifier> class <name>Foo</name> <block>{
-    <decl_stmt><decl><type><specifier>public</specifier> <name>int</name></type> <name>bar</name></decl>;</decl_stmt>
-}</block></class>";
+            string fooXml = @"<namespace>namespace <name>Example</name> <block>{
+    <class><specifier>public</specifier> class <name>Foo</name> <block>{
+        <decl_stmt><decl><type><specifier>public</specifier> <name>int</name></type> <name>bar</name></decl>;</decl_stmt>
+    }</block></class>
+}</block></namespace>";
             var fooFileUnit = fileSetup.GetFileUnitForXmlSnippet(fooXml, "Foo.cs");
             var globalScope = codeParser.ParseFileUnit(fooFileUnit);
 
             Assert.AreEqual(1, globalScope.ChildScopes.Count());
-            var foo = globalScope.ChildScopes.First() as TypeDefinition;
+            var example = globalScope.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(example);
+            Assert.AreEqual("Example", example.Name);
+            Assert.AreEqual(1, example.ChildScopes.Count());
+            var foo = example.ChildScopes.First() as TypeDefinition;
+            Assert.IsNotNull(foo);
+            Assert.AreEqual("Foo", foo.Name);
+            Assert.AreEqual(0, foo.ChildScopes.Count());
+            Assert.AreEqual(1, foo.DeclaredVariables.Count());
+        }
+
+        [Test]
+        public void TestCreateTypeDefinition_CompoundNamespace() {
+            ////Foo.cs
+            //namespace Example.Level2.Level3 {
+            //    public class Foo {
+            //        public int bar;
+            //    }
+            //}
+            string fooXml = @"<namespace>namespace <name><name>Example</name><op:operator>.</op:operator><name>Level2</name><op:operator>.</op:operator><name>Level3</name></name> <block>{
+    <class><specifier>public</specifier> class <name>Foo</name> <block>{
+        <decl_stmt><decl><type><specifier>public</specifier> <name>int</name></type> <name>bar</name></decl>;</decl_stmt>
+    }</block></class>
+}</block></namespace>";
+            var fooFileUnit = fileSetup.GetFileUnitForXmlSnippet(fooXml, "Foo.cs");
+            var globalScope = codeParser.ParseFileUnit(fooFileUnit);
+
+            Assert.AreEqual(1, globalScope.ChildScopes.Count());
+            var example = globalScope.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(example);
+            Assert.AreEqual("Example", example.Name);
+            Assert.AreEqual(1, example.ChildScopes.Count());
+            var level2 = example.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(level2);
+            Assert.AreEqual("Level2", level2.Name);
+            Assert.AreEqual(1, level2.ChildScopes.Count());
+            var level3 = level2.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(level3);
+            Assert.AreEqual("Level3", level3.Name);
+            Assert.AreEqual(1, level3.ChildScopes.Count());
+            var foo = level3.ChildScopes.First() as TypeDefinition;
+            Assert.IsNotNull(foo);
+            Assert.AreEqual("Foo", foo.Name);
+            Assert.AreEqual(0, foo.ChildScopes.Count());
+            Assert.AreEqual(1, foo.DeclaredVariables.Count());
+        }
+
+        [Test]
+        public void TestCreateTypeDefinition_NestedCompoundNamespace() {
+            ////Foo.cs
+            //namespace Watermelon {
+            //    namespace Example.Level2.Level3 {
+            //        public class Foo {
+            //            public int bar;
+            //        }
+            //    }
+            //}
+            string fooXml = @"<namespace>namespace <name>Watermelon</name> <block>{
+    <namespace>namespace <name><name>Example</name><op:operator>.</op:operator><name>Level2</name><op:operator>.</op:operator><name>Level3</name></name> <block>{
+        <class><specifier>public</specifier> class <name>Foo</name> <block>{
+            <decl_stmt><decl><type><specifier>public</specifier> <name>int</name></type> <name>bar</name></decl>;</decl_stmt>
+        }</block></class>
+    }</block></namespace>
+}</block></namespace>";
+            var fooFileUnit = fileSetup.GetFileUnitForXmlSnippet(fooXml, "Foo.cs");
+            var globalScope = codeParser.ParseFileUnit(fooFileUnit);
+
+            Assert.AreEqual(1, globalScope.ChildScopes.Count());
+            var watermelon = globalScope.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(watermelon);
+            Assert.AreEqual("Watermelon", watermelon.Name);
+            Assert.AreEqual(1, watermelon.ChildScopes.Count());
+            var example = watermelon.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(example);
+            Assert.AreEqual("Example", example.Name);
+            Assert.AreEqual(1, example.ChildScopes.Count());
+            var level2 = example.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(level2);
+            Assert.AreEqual("Level2", level2.Name);
+            Assert.AreEqual(1, level2.ChildScopes.Count());
+            var level3 = level2.ChildScopes.First() as NamespaceDefinition;
+            Assert.IsNotNull(level3);
+            Assert.AreEqual("Level3", level3.Name);
+            Assert.AreEqual(1, level3.ChildScopes.Count());
+            var foo = level3.ChildScopes.First() as TypeDefinition;
             Assert.IsNotNull(foo);
             Assert.AreEqual("Foo", foo.Name);
             Assert.AreEqual(0, foo.ChildScopes.Count());
