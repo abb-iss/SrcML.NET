088ba18fb4f206a3e2782be3aba466845f8e37fc
patrickfrancis <patrick.francis@us.abb.com>
2013-04-29 16:35:59 -0400

    Added bigger serialization roundtrip test, updated TestHelper to work correctly.

--- ABB.SrcML.Data.Test/SerializationTests.cs
+++ ABB.SrcML.Data.Test/SerializationTests.cs
@@ -8,8 +8,8 @@ using NUnit.Framework;
 namespace ABB.SrcML.Data.Test {
     [TestFixture]
     public class SerializationTests {
-        [TestFixtureSetUp]
-        public void ClassSetup() {
+        [SetUp]
+        public void Setup() {
             if(Directory.Exists("DataArchiveTests")) {
                 Directory.Delete("DataArchiveTests", true);
             }
@@ -20,6 +20,21 @@ namespace ABB.SrcML.Data.Test {
             var archive = new SrcMLArchive("DataArchiveTests");
             archive.AddOrUpdateFile(@"..\..\TestInputs\A.h");
             archive.AddOrUpdateFile(@"..\..\TestInputs\A.cpp");
+            var data = new DataArchive(archive);
+            data.Save(@"DataArchiveTests\saved.dar");
+
+            var newData = new DataArchive(archive);
+            newData.Load(@"DataArchiveTests\saved.dar");
+
+            Assert.IsTrue(TestHelper.ScopesAreEqual(data.GlobalScope, newData.GlobalScope));
+        }
+
+        [Test]
+        public void TestRoundtrip_Self() {
+            var archive = new SrcMLArchive("DataArchiveTests");
+            foreach(var csFile in Directory.GetFiles(@"..\..\ABB.SrcML", "*.cs", SearchOption.AllDirectories)) {
+                archive.AddOrUpdateFile(csFile);
+            }
             var data = new DataArchive(archive);
             data.Save(@"DataArchiveTests\saved.dar");
 
