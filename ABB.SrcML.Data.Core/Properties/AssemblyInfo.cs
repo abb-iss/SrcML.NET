fdd07cca0ac243cb4192c0c19dffbe0b4eb4f14a
Vinay Augustine <vinay.augustine@us.abb.com>
2013-10-22 14:14:21 -0400

    Made ABB.SrcML.Data.Core CLS compliant.

--- ABB.SrcML.Data.Core/Properties/AssemblyInfo.cs
+++ ABB.SrcML.Data.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Reflection;
+﻿/******************************************************************************
+ * Copyright (c) 2013 ABB Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Vinay Augustine (ABB Group) - initial API, implementation, & documentation
+ *****************************************************************************/
+
+using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,13 +19,17 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ABB.SrcML.Data.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ABB")]
 [assembly: AssemblyProduct("ABB.SrcML.Data.Core")]
+[assembly: AssemblyDescription("Core functionality for ABB SrcML Data")]
 [assembly: AssemblyCopyright("Copyright © ABB 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,6 +38,9 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("664c6649-abfb-4884-b890-d8f48aa9f046")]
+
+[assembly: AssemblyCompany("ABB")]
+[assembly: NeutralResourcesLanguage("en-US")]
 
 // Version information for an assembly consists of the following four values:
 //
