a153146dc3d54296d94b588b09f02df3d5dfdf2e
Vinay Augustine <vinay.augustine@us.abb.com>
2013-12-13 10:10:12 -0500

    Updated MethodCallSurvey project to use the global SolutionInfo for versioning.

--- Tools/ABB.SrcML.Tools.MethodCallSurvey/Properties/AssemblyInfo.cs
+++ Tools/ABB.SrcML.Tools.MethodCallSurvey/Properties/AssemblyInfo.cs
@@ -9,8 +9,6 @@ using System.Windows;
 // associated with an assembly.
 [assembly: AssemblyTitle("ABB.SrcML.Tools.MethodCallSurvey")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ABB")]
 [assembly: AssemblyProduct("ABB.SrcML.Tools.MethodCallSurvey")]
 [assembly: AssemblyCopyright("Copyright © ABB 2013")]
 [assembly: AssemblyTrademark("")]
@@ -40,16 +38,3 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
