ce7e95f4f3a35e613cf6845e89a62c96571e5fb4
Vinay Augustine <vinay.augustine@us.abb.com>
2014-03-06 14:48:00 -0500

    Added sample queries based on existing functions in the DataRepository.

---
+++ ABB.SrcML.Data/Queries/FindMethodCallsAtLocationQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB.SrcML.Data.Queries {
+    public class FindMethodCallsAtLocationQuery : AbstractQuery<SourceLocation, Collection<IMethodCall>> {
+        public FindMethodCallsAtLocationQuery(IDataRepository data, int lockTimeout, TaskFactory factory)
+            : base(data, lockTimeout, factory) { }
+
+        protected override Collection<IMethodCall> ExecuteImpl(SourceLocation parameter) {
+            var globalScope = Data.GetGlobalScope();
+            if(globalScope != null) {
+                var scope = globalScope.GetScopeForLocation(parameter);
+                if(scope != null) {
+                    var calls = scope.MethodCalls.Where(mc => mc.Location.Contains(parameter));
+                    return new Collection<IMethodCall>(calls.OrderByDescending(mc => mc.Location, new SourceLocationComparer()).ToList());
+                }
+            }
+            return new Collection<IMethodCall>();
+        }
+
+        private class SourceLocationComparer : Comparer<SourceLocation> {
+
+            public override int Compare(SourceLocation x, SourceLocation y) {
+                if(object.Equals(x, y))
+                    return 0;
+                if(x == null && y != null)
+                    return -1;
+                if(x != null && y == null)
+                    return 1;
+
+                var result = x.StartingLineNumber.CompareTo(y.StartingLineNumber);
+                if(result == 0) {
+                    result = x.StartingColumnNumber.CompareTo(y.StartingColumnNumber);
+                }
+                return result;
+            }
+        }
+    }
+}
