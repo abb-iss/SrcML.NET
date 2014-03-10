33d18b193a1c41989eb2cd46f0c461f251ed0263
patrickfrancis <patrick.francis@us.abb.com>
2014-03-10 16:35:25 -0400

    Initial commit of new expression parsing classes

---
+++ ABB.SrcML.Data/WhileStatement.cs
@@ -0,0 +1,20 @@
+﻿/******************************************************************************
+ * Copyright (c) 2014 ABB Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Patrick Francis (ABB Group) - initial API, implementation, & documentation
+ *    Vinay Augustine (ABB Group) - initial API, implementation, & documentation
+ *****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABB.SrcML.Data {
+    public class WhileStatement : ControlStatement {}
+}
