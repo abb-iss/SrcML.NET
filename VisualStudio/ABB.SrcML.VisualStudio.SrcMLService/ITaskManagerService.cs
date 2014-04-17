b8cb01a306b9335702f5e16979014be7b1336156
Vinay Augustine <vinay.augustine@us.abb.com>
2014-04-17 15:33:58 -0400

    [#69697126] Removed ABB.SrcML.VisualStudio.Interfaces. Clients should refer directly to ABB.SrcML.VisualStudio.SrcMLService.dll

--- VisualStudio/ABB.SrcML.VisualStudio.SrcMLService/ITaskManagerService.cs
+++ VisualStudio/ABB.SrcML.VisualStudio.SrcMLService/ITaskManagerService.cs
@@ -1,4 +1,15 @@
-﻿using ABB.SrcML;
+﻿/******************************************************************************
+ * Copyright (c) 2014 ABB Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Vinay Augustine (ABB Group) - Initial implementation
+ *****************************************************************************/
+
+using ABB.SrcML;
 using System;
 using System.Collections.Generic;
 using System.Linq;
