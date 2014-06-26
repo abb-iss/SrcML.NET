ee9b52c6f7df6f146ab0bbe73ea0a13d92545744
Vinay Augustine <vinay.augustine@us.abb.com>
2014-06-26 11:49:48 -0400

    Added documentation to the data file name mapping class.

--- ABB.SrcML.Data/DataFileNameMapping.cs
+++ ABB.SrcML.Data/DataFileNameMapping.cs
@@ -19,31 +19,40 @@ using System.Xml;
 using System.Xml.Linq;
 
 namespace ABB.SrcML.Data {
+    /// <summary>
+    /// File name mapping for data files
+    /// </summary>
     public class DataFileNameMapping : ShortFileNameMapping {
         private bool _compressionEnabled;
 
         /// <summary>
-        /// 
+        /// Create a new data file name mapping. This uses <see cref="XmlSerialization.DEFAULT_COMPRESSED_EXTENSION"/>
+        /// as the extension for all target files.
         /// </summary>
-        /// <param name="targetDirectory"></param>
+        /// <param name="targetDirectory">The directory for the target files</param>
         public DataFileNameMapping(string targetDirectory)
             : this(targetDirectory, true) { }
 
         /// <summary>
-        /// 
+        /// Creates a new data file name mapping
         /// </summary>
-        /// <param name="targetDirectory"></param>
-        /// <param name="compressionEnabled"></param>
+        /// <param name="targetDirectory">The directory for the target files</param>
+        /// <param name="compressionEnabled">
+        /// If true, use <see cref="XmlSerialization.DEFAULT_COMPRESSED_EXTENSION">the default compression extension</see>.
+        /// Otherwise, use <see cref="XmlSerialization.DEFAULT_EXTENSION">the default extension</see></param>.
         public DataFileNameMapping(string targetDirectory, bool compressionEnabled)
             : base(targetDirectory, GetDefaultExtension(compressionEnabled)) {
             _compressionEnabled = compressionEnabled;
         }
 
         /// <summary>
-        /// 
+        /// Reads the source path from the <paramref name="targetPath"/>.
+        /// This works by reading the XML and finding the first <see cref="SrcMLLocation.XmlName">Location</see> element.
+        /// It then gets the <see cref="SourceLocation.XmlFileAttributeName">file attribute</see> within it. If the file
+        /// is compressed, it should automatically decompress it.
         /// </summary>
-        /// <param name="targetPath"></param>
-        /// <returns></returns>
+        /// <param name="targetPath">The target path</param>
+        /// <returns>The source path found in <paramref name="targetPath"/></returns>
         protected override string GetSourcePathFromTargetFile(string targetPath) {
             string sourcePath = null;
             var targetExtension = Path.GetExtension(targetPath);
