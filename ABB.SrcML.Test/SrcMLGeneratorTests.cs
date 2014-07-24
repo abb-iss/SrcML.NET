88b8f66e2c6ed74f27ee762e9f95a098e430a356
Vinay Augustine <vinay.augustine@us.abb.com>
2014-07-24 10:06:54 -0400

    Added encoding tests from Dave's commit on master.

--- ABB.SrcML.Test/SrcMLGeneratorTests.cs
+++ ABB.SrcML.Test/SrcMLGeneratorTests.cs
@@ -63,6 +63,10 @@ namespace LoggingTransformation
 }
 ");
             File.WriteAllText("srcmltest\\File with spaces.cpp", String.Format(@"int foo() {{{0}    printf(""hello world!"");{0}}}", Environment.NewLine));
+
+            Directory.CreateDirectory("srcmltest\\BadPath™");
+            File.WriteAllText("srcmltest\\BadPath™\\badPathTest.c", String.Format(@"int foo() {{{0}printf(""hello world!"");{0}}}", Environment.NewLine));
+            File.WriteAllText("srcmltest\\fooBody.c", String.Format(@"int foo() {{{0}printf(""hello world!™"");{0}}}", Environment.NewLine));
         }
 
         [TestFixtureTearDown]
@@ -90,6 +94,14 @@ namespace LoggingTransformation
         public void DifferentLanguageTest() {
             generator.GenerateSrcMLFromFile("srcmltest\\CSHARP.cs", "srcml_xml\\differentlanguage_java.xml", Language.Java);
             var doc = new SrcMLFile("srcml_xml\\differentlanguage_java.xml");
+            Assert.IsNotNull(doc);
+        }
+
+        [Test, Category("SrcMLUpdate")]
+        public void TestStrangeEncodings([Values(@"srcmltest\BadPath™\badPathTest.c", @"srcmltest\fooBody.c")] string sourceFileName) {
+            var xmlFileName = Path.Combine("srcml_xml", Path.GetFileName(Path.ChangeExtension(sourceFileName, ".xml")));
+            generator.GenerateSrcMLFromFile(sourceFileName, xmlFileName, Language.C);
+            var doc = new SrcMLFile(xmlFileName);
             Assert.IsNotNull(doc);
         }
 
