d54f68c4ad4ef26d520788437bd7c4d0e1530242
Vinay Augustine <vinay.augustine@us.abb.com>
2014-08-20 12:52:55 -0400

    Updated multi-threaded access tests to use the new AbstractWorkingSet and DataProject.

--- ABB.SrcML.Data.Test/MultithreadedAccessTests.cs
+++ ABB.SrcML.Data.Test/MultithreadedAccessTests.cs
@@ -13,63 +13,56 @@ namespace ABB.SrcML.Data.Test {
     [Category("Build")]
     public class MultithreadedAccessTests {
 
-        //[Test]
-        //public void TestQueryDuringUpdate() {
-        //    var sourceFolder = "TestQueryDuringUpdate";
-        //    var dataFolder = "TestQueryDuringUpdate_Data";
-        //    Directory.CreateDirectory(sourceFolder);
-        //    string fooSourcePath = Path.Combine(sourceFolder, "foo.cpp");
-        //    string barSourcePath = Path.Combine(sourceFolder, "bar.cpp");
+        [Test]
+        public void TestQueryDuringUpdate() {
+            var sourceFolder = "TestQueryDuringUpdate";
+            var dataFolder = "TestQueryDuringUpdate_Data";
+            Directory.CreateDirectory(sourceFolder);
+            string fooSourcePath = Path.Combine(sourceFolder, "foo.cpp");
+            string barSourcePath = Path.Combine(sourceFolder, "bar.cpp");
 
-        //    var fooRevisions = new string[] { "void foo() { }", "void foo() { bar(); }" };
+            var fooRevisions = new string[] { "void foo() { }", "void foo() { bar(); }" };
 
-        //    File.WriteAllText(fooSourcePath, fooRevisions[1]);
-        //    File.WriteAllText(barSourcePath, "void bar() { }");
+            File.WriteAllText(fooSourcePath, fooRevisions[1]);
+            File.WriteAllText(barSourcePath, "void bar() { }");
 
-        //    int iterations = 1000;
-        //    using(var archive = new SrcMLArchive(dataFolder)) {
-        //        using(var data = new DataRepository(archive)) {
-        //            data.InitializeData();
-        //            archive.AddOrUpdateFile(Path.Combine(sourceFolder, "foo.cpp"));
-        //            archive.AddOrUpdateFile(Path.Combine(sourceFolder, "bar.cpp"));
+            int iterations = 1000;
+            using(var project = new DataProject<CompleteWorkingSet>(dataFolder, sourceFolder, "SrcML")) {
+                project.Update();
+                project.StartMonitoring();
 
-        //            var developer = new Task(() => {
-        //                for(int i = 0; i < iterations; i++) {
-        //                    File.WriteAllText(fooSourcePath, fooRevisions[i % 2]);
-        //                    archive.AddOrUpdateFile(fooSourcePath);
-        //                }
-        //            });
+                var developer = new Task(() => {
+                    for(int i = 0; i < iterations; i++) {
+                        File.WriteAllText(fooSourcePath, fooRevisions[i % 2]);
+                    }
+                });
 
-        //            developer.Start();
-        //            Assert.DoesNotThrow(() => {
-        //                for(int i = 0; i < iterations; i++) {
-        //                    var foo = GetMethodWithName(data, 500, "foo");
-        //                    Thread.Sleep(1);
-        //                    var bar = GetMethodWithName(data, 500, "bar");
+                developer.Start();
+                Assert.DoesNotThrow(() => {
+                    for(int i = 0; i < iterations; i++) {
+                        var foo = GetMethodWithName(project.WorkingSet, 500, "foo");
+                        var bar = GetMethodWithName(project.WorkingSet, 500, "bar");
+                        foo.ContainsCallTo(bar);
+                        if(i % 10 == 0 && i > 0) {
+                            Console.WriteLine("Finished {0} iterations", i);
+                        }
+                    }
+                });
+                developer.Wait();
+            }
+        }
 
-        //                    foo.ContainsCallTo(bar);
-        //                    if(i % 10 == 0 && i > 0) {
-        //                        Console.WriteLine("Finished {0} iterations", i);
-        //                    }
-        //                }
-        //            });
+        private static MethodDefinition GetMethodWithName(AbstractWorkingSet workingSet, int timeout, string methodName) {
+            MethodDefinition result = null;
+            NamespaceDefinition globalScope;
+            Assert.That(workingSet.TryObtainReadLock(timeout, out globalScope));
+            try {
+                result = globalScope.GetNamedChildren<MethodDefinition>("foo").FirstOrDefault();
+                return result;
+            } finally {
+                workingSet.ReleaseReadLock();
+            }
 
-        //            developer.Wait();
-        //        }
-        //    }
-        //}
-
-        //private static IMethodDefinition GetMethodWithName(IDataRepository dataRepository, int timeout, string methodName) {
-        //    IMethodDefinition result = null;
-        //    IScope globalScope;
-        //    Assert.That(dataRepository.TryLockGlobalScope(timeout, out globalScope));
-        //    try {
-        //        result = globalScope.GetChildScopesWithId<IMethodDefinition>("foo").FirstOrDefault();
-        //        return result;
-        //    } finally {
-        //        dataRepository.ReleaseGlobalScopeLock();
-        //    }
-            
-        //}
+        }
     }
 }
