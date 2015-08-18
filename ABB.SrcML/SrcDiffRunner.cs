95db8f257a4f75e6be46ab5e081fe5384b947271
Christian Newman <cnewman@kent.edu>
2015-08-18 17:37:08 -0400

    Adding file for srcDiff runner

---
+++ ABB.SrcML/SrcDiffRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ABB.SrcML.Utilities;
+
+namespace ABB.SrcML
+{
+    class SrcDiffRunner : ExecutableRunner
+    {
+
+    }
+}
