02f5c041a3e73487363bd735d5ba5278fc137e84
Christian Newman <cnewman@kent.edu>
2015-08-27 11:26:54 -0400

    Revert "Removing a file I didn't mean to add."
    This reverts commit 46a622311a6627fcef1c7aa4540f0ff092b0d60c.

---
+++ SolutionInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("ABB")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("99.99.489.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyInformationalVersion("99.99.489.0-Debug-9cf3cbd0ecdaeab59bf6af507b33188b7a8cc31e")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("99.99.489.0-Release-9cf3cbd0ecdaeab59bf6af507b33188b7a8cc31e")]
+#endif
