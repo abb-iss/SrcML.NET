352ffb25ac2cbfb0bd97d45121dc0f34f84c51ad
Patrick Francis <patrick.francis@us.abb.com>
2015-09-18 17:13:11 -0400

    Updated LibSrcMLWrapper project to use a SolutionInfo.cpp file that's generated by msbuild.

--- LibSrcMLWrapper/AssemblyInfo.cpp
+++ LibSrcMLWrapper/AssemblyInfo.cpp
@@ -13,25 +13,11 @@ using namespace System::Security::Permissions;
 //
 [assembly:AssemblyTitleAttribute(L"LibSrcMLWrapper")];
 [assembly:AssemblyDescriptionAttribute(L"")];
-[assembly:AssemblyConfigurationAttribute(L"")];
-[assembly:AssemblyCompanyAttribute(L"")];
 [assembly:AssemblyProductAttribute(L"LibSrcMLWrapper")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2015")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-
-[assembly:AssemblyVersionAttribute("1.0.*")];
 
 [assembly:ComVisible(false)];
 
