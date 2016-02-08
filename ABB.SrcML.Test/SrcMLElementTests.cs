d822854e58d8352491ad5a559688b238f27192f9
Christian Newman <cnewman@kent.edu>
2016-02-08 18:06:38 -0500

    These tests weren't in srcml.data but I converted them anyway.

--- ABB.SrcML.Test/SrcMLElementTests.cs
+++ ABB.SrcML.Test/SrcMLElementTests.cs
@@ -17,6 +17,7 @@ using System.Xml;
 using System.Xml.Linq;
 using NUnit.Framework;
 using ABB.SrcML;
+using System.Collections.ObjectModel;
 
 namespace ABB.SrcML.Test {
     /// <summary>
@@ -32,9 +33,9 @@ namespace ABB.SrcML.Test {
             //construct the necessary srcML wrapper unit tags
             XmlNamespaceManager xnm = SrcML.NamespaceManager;
             StringBuilder namespaceDecls = new StringBuilder();
-            foreach(string prefix in xnm) {
-                if(prefix != string.Empty && !prefix.StartsWith("xml", StringComparison.OrdinalIgnoreCase)) {
-                    if(prefix.Equals("src", StringComparison.OrdinalIgnoreCase)) {
+            foreach (string prefix in xnm) {
+                if (prefix != string.Empty && !prefix.StartsWith("xml", StringComparison.OrdinalIgnoreCase)) {
+                    if (prefix.Equals("src", StringComparison.OrdinalIgnoreCase)) {
                         namespaceDecls.AppendFormat("xmlns=\"{0}\" ", xnm.LookupNamespace(prefix));
                     } else {
                         namespaceDecls.AppendFormat("xmlns:{0}=\"{1}\" ", prefix, xnm.LookupNamespace(prefix));
@@ -46,52 +47,57 @@ namespace ABB.SrcML.Test {
 
         [Test]
         public void TestGetMethodSignature_Normal() {
-            string testSrcML = @"<function><type><name>char</name><type:modifier>*</type:modifier></type> <name><name>MyClass</name><op:operator>::</op:operator><name>foo</name></name><parameter_list>(<param><decl><type><name>int</name></type> <name>bar</name></decl></param>)</parameter_list> <block>{
-    <if>if<condition>(<expr><name>bar</name> <op:operator>&gt;</op:operator> <call><name>GetNumber</name><argument_list>()</argument_list></call></expr>)</condition><then> <block>{
-        <return>return <expr><lit:literal type=""string"">""Hello, world!""</lit:literal></expr>;</return>
-    }</block></then> <else>else <block>{
-        <return>return <expr><lit:literal type=""string"">""Goodbye cruel world!""</lit:literal></expr>;</return>
-    }</block></else></if>
-}</block></function>";
-            XElement xml = XElement.Parse(string.Format(srcMLFormat, testSrcML), LoadOptions.PreserveWhitespace);
+            string testSrcML = @"char* MyClass::foo(int bar) {
+    if(bar > GetNumber()) {
+        return 'Hello, world!';
+    } else {
+        return 'Goodbye cruel world!';
+    }";
+            LibSrcMLRunner runA = new LibSrcMLRunner();
+            string srcMLA = runA.GenerateSrcMLFromString(testSrcML, "A.cs", Language.CSharp, new Collection<UInt32>() { LibSrcMLRunner.SrcMLOptions.SRCML_OPTION_POSITION }, false);
+            XElement xml = XElement.Parse(srcMLA, LoadOptions.PreserveWhitespace);
 
-            string actual = SrcMLElement.GetMethodSignature(xml.Element(SRC.Function));
+            string actual = SrcMLElement.GetMethodSignature(xml.Descendants(SRC.Function).First());
             string expected = "char* MyClass::foo(int bar)";
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void TestGetMethodSignature_Whitespace() {
-            string testSrcML = @"<function><type><name>char</name><type:modifier>*</type:modifier></type> <name><name>MyClass</name><op:operator>::</op:operator><name>foo</name></name><parameter_list>(
-	<param><decl><type><name>int</name></type> <name>bar</name></decl></param>,
-	<param><decl><type><name>int</name></type> <name>baz</name></decl></param>,
-	<param><decl><type><name>float</name></type> <name>xyzzy</name></decl></param>)</parameter_list> 
-<block>{
-    <if>if<condition>(<expr><name>bar</name> <op:operator>&gt;</op:operator> <call><name>GetNumber</name><argument_list>()</argument_list></call></expr>)</condition><then> <block>{
-        <return>return <expr><lit:literal type=""string"">""Hello, world!""</lit:literal></expr>;</return>
-    }</block></then> <else>else <block>{
-        <return>return <expr><lit:literal type=""string"">""Goodbye cruel world!""</lit:literal></expr>;</return>
-    }</block></else></if>
-}</block></function>";
-            XElement xml = XElement.Parse(string.Format(srcMLFormat, testSrcML), LoadOptions.PreserveWhitespace);
+            string testSrcML = @"char* MyClass::foo(
+        int bar,
+        int baz,
+        float xyzzy)
+{
+    if(bar > GetNumber()) {
+        return 'Hello, world!';
+    } else {
+        return 'Goodbye cruel world!';
+    }
+}";
+            LibSrcMLRunner runA = new LibSrcMLRunner();
+            string srcMLA = runA.GenerateSrcMLFromString(testSrcML, "A.cs", Language.CSharp, new Collection<UInt32>() { LibSrcMLRunner.SrcMLOptions.SRCML_OPTION_POSITION }, false);
+            XElement xml = XElement.Parse(srcMLA, LoadOptions.PreserveWhitespace);
 
-            string actual = SrcMLElement.GetMethodSignature(xml.Element(SRC.Function));
+            string actual = SrcMLElement.GetMethodSignature(xml.Descendants(SRC.Function).First());
             string expected = "char* MyClass::foo( int bar, int baz, float xyzzy)";
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void TestGetMethodSignature_InitializerList() {
-            string testSrcML = @"<constructor><name><name>MyClass</name><op:operator>::</op:operator><name>MyClass</name></name><parameter_list>(<param><decl><type><name>int</name></type> <name>bar</name></decl></param>)</parameter_list> <member_list>: <call><name>_capacity</name><argument_list>(<argument><expr><lit:literal type=""number"">15</lit:literal></expr></argument>)</argument_list></call>, <call><name>_len</name><argument_list>(<argument><expr><lit:literal type=""number"">0</lit:literal></expr></argument>)</argument_list></call> </member_list><block>{
-    <if>if<condition>(<expr><name>bar</name> <op:operator>&gt;</op:operator> <call><name>GetNumber</name><argument_list>()</argument_list></call></expr>)</condition><then> <block>{
-        <return>return <expr><lit:literal type=""string"">""Hello, world!""</lit:literal></expr>;</return>
-    }</block></then> <else>else <block>{
-        <return>return <expr><lit:literal type=""string"">""Goodbye cruel world!""</lit:literal></expr>;</return>
-    }</block></else></if>
-}</block></constructor>";
-            XElement xml = XElement.Parse(string.Format(srcMLFormat, testSrcML), LoadOptions.PreserveWhitespace);
+            string testSrcML = @"MyClass::MyClass(int bar) : _capacity(15), _len(0) {
+    if(bar > GetNumber()) {
+        return 'Hello, world!';
+    } else {
+        return 'Goodbye cruel world!';
+    }
+}";
+            LibSrcMLRunner runA = new LibSrcMLRunner();
+            string srcMLA = runA.GenerateSrcMLFromString(testSrcML, "A.cs", Language.CSharp, new Collection<UInt32>() { LibSrcMLRunner.SrcMLOptions.SRCML_OPTION_POSITION }, false);
+            XElement xml = XElement.Parse(srcMLA, LoadOptions.PreserveWhitespace);
 
-            string actual = SrcMLElement.GetMethodSignature(xml.Element(SRC.Constructor));
+            string actual = SrcMLElement.GetMethodSignature(xml.Descendants(SRC.Constructor).First());
             string expected = "MyClass::MyClass(int bar)";
             Assert.AreEqual(expected, actual);
         }
